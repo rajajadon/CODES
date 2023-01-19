10551c01e621f6e91d8f832b98a819542962f1df
rajajadon <123127577+rajajadon@users.noreply.github.com>
2023-01-20 04:29:07 +0530

    Update Forst.py

--- Forst.py
+++ Forst.py
@@ -1,7 +1,7 @@
 import streamlit 
 import json
 st.file_uploader('upload the file',type=['pdf']
-st.button(submit)
+st.button('submit')
 # Open the JSON file
 with open('data.json', 'r') as file:
     # Load the data from the file
